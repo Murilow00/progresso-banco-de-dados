16fb064c2472d5c57b72929a9098fb7754856327
Murilow <milanmurilo7@gmail.com>
2025-09-15 15:58:40 -0300

    Cria as querys de create

--- queries/01-insert-artist.sql
+++ queries/01-insert-artist.sql
@@ -11,3 +11,17 @@ CREATE TABLE artistas (
     musica_famosa VARCHAR(50),
     ativa BOOLEAN 
 );
+
+
+INSERT INTO artistas (nome, tipo, membros, genero_musical, ano_lancamento, pais_de_origem, musica_famosa, ativa)
+VALUES
+('Gorillaz', 'banda', 4, 'alternativo', 1998, 'UK', 'Feel Good Inc.', TRUE),
+('Lovejoy', 'banda', 4, 'indie rock', 2021, 'UK', 'Call Me What You Like', TRUE),
+('Wilbur Soot', 'solo', 1, 'indie rock', 2017, 'UK', 'Your New Boyfriend', TRUE),
+('Daft Punk', 'banda', 2, 'eletrônico', 1993, 'França', 'Get Lucky', FALSE),
+('Arctic Monkeys', 'banda', 4, 'indie rock', 2002, 'UK', 'Do I Wanna Know?', TRUE),
+('The Strokes', 'banda', 5, 'garage rock', 1998, 'USA', 'Last Nite', TRUE),
+('Radiohead', 'banda', 5, 'rock alternativo', 1985, 'UK', 'Creep', TRUE),
+('Billie Eilish', 'solo', 1, 'pop alternativo', 2015, 'USA', 'Bad Guy', TRUE),
+('Paramore', 'banda', 3, 'pop punk', 2004, 'USA', 'Misery Business', TRUE),
+('Tame Impala', 'banda', 1, 'psicodélico', 2007, 'Austrália', 'The Less I Know the Better', TRUE);
---
+++ queries/select.sql
@@ -0,0 +1,9 @@
+SELECT * FROM artistas;
+
+SELECT COUNT(*) AS total_artistas  FROM artistas
+
+SELECT nome, genero_musical FROM artistas
+WHERE pais_de_origem = 'UK'
+
+SELECT nome, genero_musical FROM artistas
+WHERE id = 8 AND 1

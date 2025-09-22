baba8e3db9ab609738c34021279fc27be1020d53
Murilow <milanmurilo7@gmail.com>
2025-09-22 09:00:05 -0300

    finaliza os comandos basicos

---
+++ queries/03-atividades.sql
@@ -0,0 +1,9 @@
+UPDATE usuarios SET username = 'Progamer 2025'
+WHERE username = 'NoobMaster';
+
+SELECT * FROM usuarios
+
+UPDATE usuarios SET pontos = pontos + 1000;
+
+UPDATE usuarios SET email = 'main@gmail.com'
+WHERE email = 'master@email.com ';
